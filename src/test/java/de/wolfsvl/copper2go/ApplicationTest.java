c50010487883ad39bd9d9d118caf99512ffd69bb
wsluyterman <wolf.sluyterman@scoop-software.de>
2020-08-02 16:12:46 +0200

    Fix test bug for unix.

--- src/test/java/de/wolfsvl/copper2go/ApplicationTest.java
+++ src/test/java/de/wolfsvl/copper2go/ApplicationTest.java
@@ -17,8 +17,8 @@ class ApplicationTest {
         System.setOut(new PrintStream(byteArrayOutputStream));
         System.setIn(new ByteArrayInputStream("Wolf\r\nexit\r\n".getBytes()));
         Application.main(null);
-        final String start = "Enter your name: \r\n" + "HEllo Wolf! (Fix the bug;-)";
-        final String result = byteArrayOutputStream.toString(StandardCharsets.UTF_8);
+        final String start = "Enter your name: \n" + "HEllo Wolf! (Fix the bug;-)";
+        final String result = byteArrayOutputStream.toString(StandardCharsets.UTF_8).replace("\r", "");
         Assertions.assertTrue(result.length() > start.length(), "Longer result.");
         Assertions.assertEquals(start, result.substring(0,start.length()), "Dialog starts as expected.");
     }
