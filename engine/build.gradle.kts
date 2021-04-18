9dd83154a69747746a0d3439e22fefd2b62d82a5
wsluyterman <wolf.sluyterman@scoop-software.de>
2021-04-18 17:51:17 +0200

    Use exact copper GAV.

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -1,9 +1,9 @@
 dependencies {
     implementation(project(":copper2go-api"))
 
-    implementation("org.copper-engine:copper-ext:5+")
-    implementation("org.copper-engine:copper-coreengine:5+")
-    implementation("org.copper-engine:copper-jmx-interface:5+")
+    implementation("org.copper-engine:copper-coreengine:5.3.0")
+    implementation("org.copper-engine:copper-jmx-interface:5.3.0")
+    implementation("org.copper-engine:copper-ext:5.3.0")
 
     // needed for exceptions
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.7.0.202003110725-r")
