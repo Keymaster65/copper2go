aba77f7e3e78b278aece8ae646e59dbb542b244d
wsluyterman <wolf.sluyterman@scoop-software.de>
2022-01-21 22:02:38 +0100

    Log exception in workflowInstance

--- vanilla-engine/src/main/java/io/github/keymaster65/copper2go/engine/vanilla/PayloadReceiverImpl.java
+++ vanilla-engine/src/main/java/io/github/keymaster65/copper2go/engine/vanilla/PayloadReceiverImpl.java
@@ -19,6 +19,8 @@ import io.github.keymaster65.copper2go.api.connector.EngineException;
 import io.github.keymaster65.copper2go.api.connector.PayloadReceiver;
 import io.github.keymaster65.copper2go.api.connector.ReplyChannel;
 import io.github.keymaster65.copper2go.api.workflow.WorkflowData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
@@ -26,6 +28,9 @@ import java.util.Map;
 import java.util.UUID;
 
 public class PayloadReceiverImpl implements PayloadReceiver {
+
+    private static final Logger log = LoggerFactory.getLogger(PayloadReceiverImpl.class);
+
     private final VanillaEngineImpl vanillaEngineImpl;
 
     public PayloadReceiverImpl(final VanillaEngineImpl vanillaEngineImpl) {
@@ -34,15 +39,20 @@ public class PayloadReceiverImpl implements PayloadReceiver {
 
     @Override
     public void receive(final String payload, final Map<String, String> attributes, final ReplyChannel replyChannel, final String workflow, final long major, final long minor) throws EngineException {
-        Workflow processInstance;
+        Workflow workflowInstance;
         try {
-            processInstance = createWorkflowInstance(workflow, major, minor);
+            workflowInstance = createWorkflowInstance(workflow, major, minor);
         } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
             throw new EngineException("Can't create workflow instance.", e);
         }
         WorkflowData workflowData = storeReplyChannel(payload, attributes, replyChannel);
-        vanillaEngineImpl.executorService.submit(() ->
-                processInstance.main(workflowData)
+        vanillaEngineImpl.executorService.submit(() -> {
+                    try {
+                        workflowInstance.main(workflowData);
+                    } catch (Exception e) {
+                        log.error("Exception in workflowInstance.", e);
+                    }
+                }
         );
     }
 
