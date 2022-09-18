9cb2262ae3a7df786de906da43dd34cb0eb83ced
wsluyterman <wolf.sluyterman@scoop-software.de>
2022-09-18 13:20:28 +0200

    Fix encoding smell

--- copper2go-app/src/main/java/io/github/keymaster65/copper2go/application/Copper2GoApplication.java
+++ copper2go-app/src/main/java/io/github/keymaster65/copper2go/application/Copper2GoApplication.java
@@ -27,6 +27,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -64,7 +65,7 @@ public class Copper2GoApplication implements Application {
 
     public synchronized void startWithStdInOut() throws EngineException, StandardInOutException {
         start();
-        final var standardInOutReceiver = new StandardInOutReceiver(new BufferedReader(new InputStreamReader(System.in)));
+        final var standardInOutReceiver = new StandardInOutReceiver(new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8)));
         standardInOutReceiver.listenLocalStream(copper2GoEngine.payloadReceiver());
     }
 
