05e9b3bd70c17a89bc38345a1e24bd9e9f66dfd3
wsluyterman <wolf.sluyterman@scoop-software.de>
2022-12-23 12:34:53 +0100

    Improved instructions incl. key expiry key

--- release-api.bash
+++ release-api.bash
@@ -5,6 +5,7 @@
 # 2) Signing
 # 2.1) Start this script
 # 2.2) Enter passphrase for CA-Key (Kleopatra)
+# 2.3) If key is expired visit: https://central.sonatype.org/publish/requirements/gpg/#locate-and-examine-your-staging-repository
 # 3) Upload at https://oss.sonatype.org/#nexus-search;quick~copper2go
 # 3.1) Login as keymaster65 with Password sonartype
 # 3.2) Select "Staging Upload" or https://oss.sonatype.org/#staging-upload
@@ -12,10 +13,11 @@
 # 3.4) Select local "bundle.jar" and click "Upload Bundle"
 # 3.5) Check "staging Repositories"
 # 3.6) Check not in "https://repo1.maven.org/maven2/io/github/keymaster65/copper2go-api/"
-# 4) Press "Release" (in der Überschriftsleiste) and enter discription (may be the release notes title)
-# 4.1) Upload at https://oss.sonatype.org/#nexus-search;quick~copper2go
-# 4.2) Check later "https://search.maven.org/search?q=a:copper2go-api"
-# 4.3) Check later "https://repo1.maven.org/maven2/io/github/keymaster65/copper2go-api/"
+# 4) Press "Release" (in der Überschriftsleiste, erscheint ggfls. verzögert)
+# 4.1) Enter discription in next dialog (may be the release notes title)
+# 4.2) Upload at https://oss.sonatype.org/#nexus-search;quick~copper2go
+# 4.3) Check later "https://search.maven.org/search?q=a:copper2go-api"
+# 4.4) Check later "https://repo1.maven.org/maven2/io/github/keymaster65/copper2go-api/"
 # 5) Tag
 # 5.1) create tag in idea or git tag v3.2.0-api
 # 5.2) git push origin v3.2.0-api -f
