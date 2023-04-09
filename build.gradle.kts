49a0dc44564934dea9e73bfd04329bf2ff11c7bc
wsluyterman <wolf.sluyterman@scoop-software.de>
2023-04-09 14:52:06 +0200

    Updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -152,13 +152,13 @@ allprojects {
             implementation("commons-io:commons-io:2.11.0") {
                 because("Bug in 2.8.0 while deleting dirs on Windows 10; JDK11")
             }
-            implementation("net.minidev:accessors-smart:2.4.8") {
+            implementation("net.minidev:accessors-smart:2.4.9") {
                 because("Security scan found 1.2")
             }
             implementation("org.apache.httpcomponents:httpclient:4.5.14") {
                 because("Security scan found 4.5.2")
             }
-            implementation("net.minidev:json-smart:2.4.8") {
+            implementation("net.minidev:json-smart:2.4.10") {
                 because("Security scan found 2.3")
             }
             implementation("org.apache.velocity:velocity-engine-core:2.3") {
