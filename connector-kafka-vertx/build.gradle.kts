2504472e20ac9c051b294c352c61931dd9b0e30f
Keymaster65 <wolfsvl@web.de>
2023-11-18 11:10:43 +0100

    Update io.vertx 4.4.6 to 4.5.0

--- connector-kafka-vertx/build.gradle.kts
+++ connector-kafka-vertx/build.gradle.kts
@@ -6,9 +6,9 @@ dependencies {
     implementation(project(":connector-api"))
     implementation(project(":copper2go-api"))
 
-    implementation("io.vertx:vertx-core:4.4.6")
+    implementation("io.vertx:vertx-core:4.5.0")
     implementation("io.vertx:vertx-web:4.5.0")
-    implementation("io.vertx:vertx-kafka-client:4.4.6")
+    implementation("io.vertx:vertx-kafka-client:4.5.0")
 
     testImplementation("org.testcontainers:kafka:1.19.1")
 }
