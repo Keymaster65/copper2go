eb6c370fb1fc8828318353d331da874b6a803a41
Keymaster65 <wolfsvl@web.de>
2023-11-19 09:47:14 +0100

    Remove toolchain needed for gradle and JDK 20

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,10 +85,6 @@ allprojects {
     }
 
     java {
-        toolchain {
-            languageVersion = JavaLanguageVersion.of(21)
-        }
-
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
