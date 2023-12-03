03b9eaa7f1af3ac198e1f29765277bc849a117a7
Keymaster65 <wolfsvl@web.de>
2023-12-03 15:25:56 +0100

    Try delay 1000

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,7 +107,7 @@ allprojects {
         failBuildOnCVSS = 0F
         suppressionFile = "./cveSuppressionFile.xml"
         nvd.apiKey = nvdApiKey
-        nvd.delay = 100
+        nvd.delay = 1000
     }
 
     dependencyLocking {
