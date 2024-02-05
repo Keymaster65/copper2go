854a73f6eba116532e255eab6d718cc00bd9ba17
Keymaster65 <wolfsvl@web.de>
2024-02-05 16:31:43 +0100

    Serveral more updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -150,7 +150,7 @@ allprojects {
 
         testImplementation("org.assertj:assertj-assertions-generator:2.2.1")
         testImplementation("net.jqwik:jqwik:1.8.2")
-        testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+        testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
         testImplementation("org.mockito:mockito-core:5.10.0")
 
         constraints {
