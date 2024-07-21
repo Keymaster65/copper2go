d82e9304eabbd97741acdd62841f7baca15efd60
Keymaster65 <wolfsvl@web.de>
2024-07-21 13:12:21 +0200

    More updates 21.07.2024

--- build.gradle.kts
+++ build.gradle.kts
@@ -161,7 +161,7 @@ allprojects {
         implementation("org.slf4j:slf4j-api:2.0.13")
         implementation("ch.qos.logback:logback-classic:1.5.6")
 
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
 
         testImplementation("org.assertj:assertj-assertions-generator:2.2.1")
         testImplementation("net.jqwik:jqwik:1.9.0")
--- connector-kafka-vertx/build.gradle.kts
+++ connector-kafka-vertx/build.gradle.kts
@@ -6,9 +6,12 @@ dependencies {
     implementation(project(":connector-api"))
     implementation(project(":copper2go-api"))
 
-    implementation("io.vertx:vertx-core:4.5.8")
-    implementation("io.vertx:vertx-web:4.5.8")
-    implementation("io.vertx:vertx-kafka-client:4.5.8")
+    implementation("io.vertx:vertx-core:4.5.9")
+    implementation("io.vertx:vertx-web:4.5.9")
+    implementation("io.vertx:vertx-kafka-client:4.5.9")
 
-    testImplementation("org.testcontainers:kafka:1.19.8")
+    testImplementation("org.testcontainers:kafka:1.20.0")
+    constraints {
+        testImplementation("org.apache.commons:commons-compress:1.26.2")
+    }
 }
