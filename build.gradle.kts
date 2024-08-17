fe28500205b820bb69b3d57bd2bcf2d4d04fdc43
Keymaster65 <wolfsvl@web.de>
2024-08-17 18:19:31 +0200

    Fail after removing allmost all but 2 plugins and 2 modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,33 +10,4 @@ group = "io.github.keymaster65"
 allprojects {
     apply(plugin = "org.unbroken-dome.test-sets")
     apply(plugin = "info.solidsoft.pitest")
-
-    repositories {
-        mavenCentral()
-        maven(
-            url = "https://oss.sonatype.org/content/groups/staging",
-        )
-    }
-
-    // https://github.com/szpak/gradle-pitest-plugin
-    pitest {
-        // check this in case of errors: https://mvnrepository.com/artifact/org.pitest/pitest-junit5-plugin
-        junit5PluginVersion.set("1.2.1")
-        timestampedReports.set(false)
-        outputFormats.set(setOf("HTML", "XML"))
-        exportLineCoverage.set(true)
-        verbose = true
-
-        reportAggregator {
-            testStrengthThreshold.set(77)
-            mutationThreshold.set(75)
-            maxSurviving.set(105)
-        }
-    }
-
-    // https://github.com/unbroken-dome/gradle-testsets-plugin
-    testSets {
-        create("integrationTest")
-        create("systemTest")
-    }
 }
